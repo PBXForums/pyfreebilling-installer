a8d614c5acc99953928c4cd071c6324be2d05407
DigiDaz <DigiDaz@users.noreply.github.com>
2017-10-17 19:49:33 +0100

    Update pre-install.sh

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get upgrade -y --force-yes
 apt-get install -y --force-yes git lsb-release ca-certificates
 
 #get the install script
-cd /usr/src && git clone https://github.com/DigiDaz/pyfreebilling-installer.git
+cd /usr/src && git clone https://github.com/DigiDaz/pyfreebilling-installer.git && cd /usr/src/pyfreebilling-installer/debian && \
 
-#change the working directory
-cd /usr/src/pyfreebilling-installer/debian
+#make the installer executable
+chmod +x install.sh

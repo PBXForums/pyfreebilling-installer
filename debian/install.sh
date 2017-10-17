d9b858b0beb9d7e7bde6f62ea7dd601d387d9232
DigiDaz <DigiDaz@users.noreply.github.com>
2017-10-17 19:38:06 +0100

    Create install.sh

---
+++ debian/install.sh
@@ -0,0 +1,6 @@
+apt-get install -y --force-yes build-essential autoconf automake libtool libtool-bin libncurses5 libncurses5-dev gawk libjpeg-dev \
+zlib1g-dev pkg-config libssl-dev libpq-dev unixodbc-dev odbc-postgresql postgresql postgresql-client libpq-dev libxml2-dev libxslt-dev \
+ntp ntpdate libapache2-mod-wsgi apache2 gcc python-setuptools python-pip libdbd-pg-perl libtext-csv-perl sqlite3 libsqlite3-dev \
+libcurl4-openssl-dev libpcre3-dev libspeex-dev libspeexdsp-dev libldns-dev libedit-dev libmemcached-dev python-psycopg2 python-dev \
+libgeoip-dev
+
